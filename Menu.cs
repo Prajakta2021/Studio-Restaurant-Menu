c0340f1f4bd364ea00a4f88ae9b06c0fb5bafc87
Prajakta <Prajakta0709@gmail.com>
2021-07-29 21:27:58 -0500

    Done

---
+++ Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public string MenuItem { get; set; }
+        public DateTime LastTimeUpdated { get; set; }
+
+      
+        public Menu(string menuItem, DateTime lastTimeUpdated)
+        {
+            MenuItem = menuItem;
+            LastTimeUpdated = lastTimeUpdated;
+        }
+    }
+}
+
---
+++ menu item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public bool IsNew { get; set; }
+
+
+        public MenuItem(string name, string description, string category, double price, bool isNew)
+        {
+            Name = name;
+            Description = description;
+            Category = category;
+            Price = price;
+            IsNew = isNew;
+        }
+
+
+
+    }
+
+
+}
